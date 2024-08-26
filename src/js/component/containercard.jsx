c34d62ef9158808b32e35a8ba57d4c301b837f8b
Juan E Gonzalez <158267755+juan007-play@users.noreply.github.com>
2024-08-26 00:50:45 +0000

    class rename for className

--- src/js/component/containercard.jsx
+++ src/js/component/containercard.jsx
@@ -5,7 +5,7 @@ const ContainerCard = () =>{
     return (
       <>    
     <div className="container ">
-        <div class="row row-cols-1 row-cols-md-4 g-3 mb-4 ">
+        <div className="row row-cols-1 row-cols-md-4 g-3 mb-4 ">
             <div className="col"><Card text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo aliquam, consequuntur amet, nemo dolorum nam esse."></Card></div>
 		    <div className="col"><Card text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo aliquam, consequuntur amet, nemo dolorum nam esse animi fuga repudiandae id facilis dolor."></Card></div>
 			<div className="col"><Card text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo aliquam, consequuntur amet, nemo dolorum nam esse."></Card></div>
